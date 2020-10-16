749fb1834be4f022215af10482fe22e1163337e6
shoval habas <shoval.h21@gmail.com>
2020-10-16 14:46:33 +0300

    add cmake

---
+++ main.c
@@ -0,0 +1,12 @@
+// #include <iostream>
+
+// int main(int, char**) {
+//     std::cout << "Hello, world!\n";
+// }
+
+#include <stdio.h>
+
+int main(){
+    printf("hello world");
+    return 0;
+}
